17472e9bff77482cd21ce81f74af0d617deef699
Jalvery20 <jesusalvarez010426@gmail.com>
2023-02-28 11:08:11 -0500

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../styles/global.scss"
+import { Layout } from '../components';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
